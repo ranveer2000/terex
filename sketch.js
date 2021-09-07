4f7d551e8fdc110e8819e3ae3154528cb68952cf
ranveer2000 <90261646+ranveer2000@users.noreply.github.com>
2021-09-07 18:45:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,20,20);
+  box.shapeColor = "teal";
+}
+function draw() 
+{
+  background("white");
+  if (keyIsDown(RIGHT_ARROW)){
+    box.position.x += 5;
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    box.position.x -= 5;
+  }
+  if (keyIsDown(UP_ARROW)){
+    box.position.y -= 5;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    box.position.y += 5;
+  }
+drawSprites()
+}
+
+
+
+
